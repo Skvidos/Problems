653701ab5520c8747cb2b6df4c5c407e46a3e000
Skvidos <79468362+Skvidos@users.noreply.github.com>
2022-04-14 15:20:40 +0600

    7 commit

---
+++ qwerty.py
@@ -0,0 +1,14 @@
+a = int(input('Введите число: '))
+
+even = 0
+odd = 0
+
+while a > 0:
+  if a % 2 == 0:
+    even +=1
+  else: 
+    odd += 1
+  a = a // 10
+
+print('Even:', even)
+print('Odd:', odd)
